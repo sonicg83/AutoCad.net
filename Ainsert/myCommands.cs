9a1f3e38ecf40aefbbe6d0139d8d6776e87e255c
sonicg83 <50348011+sonicg83@users.noreply.github.com>
2021-06-29 21:21:41 +0800

    new

---
+++ Ainsert/myCommands.cs
@@ -0,0 +1,116 @@
+﻿// (C) Copyright 2021 by  
+//
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+using System.Collections;
+
+// This line is not mandatory, but improves loading performances
+[assembly: CommandClass(typeof(Ainsert.MyCommands))]
+
+namespace Ainsert
+{
+    // This class is instantiated by AutoCAD for each document when
+    // a command is called by the user the first time in the context
+    // of a given document. In other words, non static data in this class
+    // is implicitly per-document!
+    public class MyCommands
+    {
+        // The CommandMethod attribute can be applied to any public  member 
+        // function of any public class.
+        // The function should take no arguments and return nothing.
+        // If the method is an intance member then the enclosing class is 
+        // intantiated for each document. If the member is a static member then
+        // the enclosing class is NOT intantiated.
+        //
+        // NOTE: CommandMethod has overloads where you can provide helpid and
+        // context menu.
+
+        // Modal Command with localized name
+        [CommandMethod("Ainsert")]
+        public void MyCommand() // This method can have any name
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Database db = doc.Database;
+            Editor ed = doc.Editor;
+
+            using (Transaction Trans = db.TransactionManager.StartTransaction())
+            {
+                try
+                {
+                    
+                    
+                    
+                    
+                    
+                    //获取布局列表(剔除模型空间)
+                    DBDictionary Layouts = Trans.GetObject(db.LayoutDictionaryId, OpenMode.ForRead) as DBDictionary;
+                    ArrayList Layoutlist = new ArrayList();
+                    foreach (DBDictionaryEntry item in Layouts)
+                    {
+                        if (item.Key != "Model")
+                        {
+                            Layout layoutobject = Trans.GetObject(item.Value, OpenMode.ForRead) as Layout;
+                            Layoutlist.Add(layoutobject);
+                        }
+                    }
+                  
+
+
+                        BlockTableRecord BTR = Trans.GetObject(LT.BlockTableRecordId, OpenMode.ForWrite) as BlockTableRecord;
+                        BTR.AppendEntity(VP);
+                        Trans.AddNewlyCreatedDBObject(VP, true);
+
+                        LayoutManager.Current.SetCurrentLayoutId(LT.Id);
+                        VP.On = true;
+                        //恢复视图的图层状态
+                        layerState.RestoreLayerState(VR.LayerState, VP.Id, 1, LayerStateMasks.CurrentViewport);
+                        //开始选择多段线裁剪视口
+                        TypedValue[] Filter = new TypedValue[]
+                       {
+                            new TypedValue((int)DxfCode.Operator,"<and"),
+                            new TypedValue((int)DxfCode.LayoutName,LT.LayoutName),
+                            new TypedValue((int)DxfCode.LayerName,ClipLayerName),
+                            new TypedValue((int)DxfCode.Operator,"<or"),
+                            new TypedValue((int)DxfCode.Start,"POLYLINE"),
+                            new TypedValue((int)DxfCode.Start,"POLYLINE3D"),
+                            new TypedValue((int)DxfCode.Start,"POLYLINE2D"),
+                            new TypedValue((int)DxfCode.Start,"LWPOLYLINE"),
+                            new TypedValue((int)DxfCode.Operator,"or>"),
+                            new TypedValue((int)DxfCode.Operator,"and>")
+                       };
+                        PromptSelectionResult selresult = ed.SelectAll(new SelectionFilter(Filter));
+                        if (selresult.Status == PromptStatus.OK)
+                        {
+                            ObjectId[] IDs = selresult.Value.GetObjectIds();
+                            VP.NonRectClipEntityId = IDs[0];
+                            VP.NonRectClipOn = true;
+                            //ed.WriteMessage("\n呵呵，剪裁了视口哦");
+                        }
+                        else
+                        {
+                            ed.WriteMessage("\n布局“{0}”中未找到裁剪视口的多义线！视口裁剪失败！", LT.LayoutName);
+                        }
+
+                    }
+
+                    Trans.Commit();
+                }
+                catch (Autodesk.AutoCAD.Runtime.Exception Ex)
+                {
+                    ed.WriteMessage("出错啦！{0}", Ex.ToString());
+                }
+                finally
+                {
+                    Trans.Dispose();
+
+                }
+
+            }
+        }
+
+    }
+
+}
