27f7c0118261a0a6bb7d41ed35bb603af3b79ba6
sonicg83 <50348011+sonicg83@users.noreply.github.com>
2021-08-05 18:35:15 +0800

    update

--- SetViewPort/SetViewPort/myCommands.cs
+++ SetViewPort/SetViewPort/myCommands.cs
@@ -9,14 +9,58 @@ using Autodesk.AutoCAD.EditorInput;
 using System.Collections;
 using System.Linq;
 using System.Text.RegularExpressions;
+using UtilityClass;
+using System.Xml;
+using System.IO;
+
 
 // This line is not mandatory, but improves loading performances
 [assembly: CommandClass(typeof(SetViewPort.MyCommands))]
 
 namespace SetViewPort
 {
+    public class Scale
+    {
+        #region Fields
+        private readonly double _viewsize;
+        private readonly double _sheetsize;
+        #endregion
+
+        #region Constructor
+        public Scale(double ViewSize,double SheetSize)
+        {
+            _viewsize = ViewSize;
+            _sheetsize = SheetSize;
+        }
+        #endregion
+
+        #region Properties
+        public double ScaleValue { get => _sheetsize / _viewsize; }
+        #endregion
+
+        #region Methods
+        public double GetScale()
+        {
+            double ScaleNumber = _sheetsize / _viewsize;
+            return ScaleNumber;
+        }
+        public string GetCivilScale()
+        {
+            string Civil = string.Format("1:{0}", Math.Round(1000 / (_sheetsize / _viewsize)).ToString());
+            return Civil;
+        }
+        public string GetBuildingScale()
+        {
+            string Building = string.Format("1:{0}", Math.Round(1 / (_sheetsize / _viewsize)).ToString());
+            return Building;
+        }
+        #endregion
+    }
+
     public class MyCommands
     {
+            Database db = Application.DocumentManager.MdiActiveDocument.Database;
+            Editor ed = Application.DocumentManager.MdiActiveDocument.Editor;
         //增加方向参数，向右为加，向左为减
         private Viewport GetViewport(ViewTableRecord InputView, Point2d BasePoint, double Scale,bool ToRight)
         {
@@ -39,18 +83,107 @@ namespace SetViewPort
             NewViewport.TwistAngle = InputView.ViewTwist;
             return NewViewport;
         }
+        private string GetSheetSize(string DstPath,string SheetPath)
+        {
+            string SheetSize = "NotFound";          
+            XmlDocument SheetSet = DstViewer.DstToXml(DstPath);
+            string Xpath1 = string.Format("/ AcSmDatabase / AcSmSheetSet / AcSmSubset / AcSmSheet[AcSmAcDbLayoutReference[AcSmProp = '{0}']]", SheetPath);
+            string Xpath2 = "AcSmCustomPropertyBag/AcSmCustomPropertyValue[@propname='图幅']/AcSmProp[@propname='Value']";
+            XmlNodeList NodeList = SheetSet.SelectNodes(Xpath1);
+            if(NodeList == null)
+            {
+                return SheetSize;
+            }
+            XmlNode Node = NodeList[0].SelectSingleNode(Xpath2);
+            if(Node == null || Node.InnerText == "")
+            {
+                return SheetSize;
+            }
+            SheetSize = Node.InnerText;              
+            return SheetSize;
+        }
 
         // Modal Command with localized name
-        [CommandMethod("SetViewPort")]
+        [CommandMethod("testvp")]
         public void MyCommand() // This method can have any name
         {
             // Put your command code here
-            Document doc = Application.DocumentManager.MdiActiveDocument;
-            Database db = doc.Database;
-            Editor ed = doc.Editor;
-            LayerStateManager layerState = db.LayerStateManager;
+            #region 获取CAD核心变量
+            
+            #endregion
+            #region 定义及初始化变量
+            string SheetSize = "NotFound";
+            int ScaleFlag = 1;
+            Hashtable FlagTable = new Hashtable();
+            FlagTable.Add("1", "建筑比例");
+            FlagTable.Add("2", "市政比例");
+            double SheetLength = 0;
             string ClipLayerName = "TK-视口";
+            #endregion
 
+
+            #region 获取图幅数据
+            string DwgFile = db.OriginalFileName;
+            string DstFile = Path.GetDirectoryName(DwgFile) + @"\图纸集数据文件.dst";
+            if (!new FileInfo(DstFile).Exists)
+            {
+                ed.WriteMessage("\n未找到图纸集数据文件<图纸集数据文件.dst>，请检查图纸集数据文件是否与图纸文件在同一个文件夹内，以及文件名是否正确。");
+                return;
+            }
+            try
+            {
+                SheetSize = GetSheetSize(DstFile, DwgFile);
+                if(SheetSize == "NotFound")
+                {
+                    ed.WriteMessage("\n未找到图幅的图纸自定义属性，请检查图纸集文件中的自定义属性内容。");
+                    return;
+                }
+                ed.WriteMessage("\n当前图纸图幅为<{0}>", SheetSize);
+            }
+            catch (Autodesk.AutoCAD.Runtime.Exception EX)
+            {
+                ed.WriteMessage("\n出错啦！{0}", EX.ToString());
+                return;
+            }
+            #endregion
+            
+
+            #region 读取设置文件
+            string inipath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\config.ini";
+
+            INIReader inifile = new INIReader(inipath);
+            if (inifile.ExistINIFile())
+            {
+                try
+                {
+                    string SheetList = inifile.IniReadValue("SheetList", "List");
+                    if(!SheetList.Contains(SheetSize))
+                    {
+                        ed.WriteMessage("\n配置文件的图幅列表中不包含本图的图幅格式，请检查图纸图幅属性是否正确或在配置文件中添加相关数据。");                        
+                        System.Diagnostics.Process.Start("notepad.exe", inipath);
+                        ed.WriteMessage("\n已打开配置文件！");
+                        return;
+                    }
+                    SheetLength = Convert.ToDouble(inifile.IniReadValue("Size", SheetSize));
+                    ClipLayerName = inifile.IniReadValue("ClipLayer", "name");
+
+                }
+                catch (System.Exception EX)
+                {
+                    ed.WriteMessage("出错了!" + EX.ToString());
+                    return;
+                }
+
+            }
+            else
+            {
+                ed.WriteMessage("\n未找到配置文件config.ini！，请检查插件所在文件夹。");
+                return;
+            }
+            #endregion
+
+            LayerStateManager layerState = db.LayerStateManager;
+            
             using (Transaction Trans = db.TransactionManager.StartTransaction())
             {
                 try
@@ -80,6 +213,8 @@ namespace SetViewPort
                         return;
                     }
 
+                    PromptIntegerOptions GetIntOption = new PromptIntegerOptions("\n选择");
+                    /*
                     double scale = 1;
                     PromptDoubleOptions GetNumberOption = new PromptDoubleOptions("\n输入视口比例，默认为1");
                     GetNumberOption.AllowNegative = false;
@@ -94,7 +229,7 @@ namespace SetViewPort
                     {
                         return;
                     }
-
+                    */
                     for (int i = 0; i < Layoutlist.Count; i++)
                     {
                         if (i == viewlist.Count)
@@ -117,7 +252,36 @@ namespace SetViewPort
                         }
                         
                         ViewTableRecord VR = query.First() ;
-                        Viewport VP = GetViewport(VR, new Point2d(0, 0), scale, false);
+                        Scale SheetScale = new Scale(VR.Width, SheetLength);
+                        Viewport VP = GetViewport(VR, new Point2d(0, 0), SheetScale.ScaleValue, false);
+                        #region 创建比例文字
+                        TextStyleTable Tst = (TextStyleTable)Trans.GetObject(db.TextStyleTableId, OpenMode.ForRead);
+                        ObjectId TextStyleID = new ObjectId();
+                        string StyleName = "TK-字段";
+                        if(Tst.Has(StyleName))
+                        {
+                            TextStyleID = Tst[StyleName];
+                        }
+                        else
+                        {
+                            TextStyleID = Tst["Standard"];
+                        }
+                        DBText ScaleMark = new DBText();
+                        if(ScaleFlag == 1)
+                        {
+                            ScaleMark.TextString = SheetScale.GetCivilScale();
+                        }
+                        else
+                        {
+                            ScaleMark.TextString = SheetScale.GetBuildingScale();
+                        }
+                        ScaleMark.TextStyleId = TextStyleID;
+                        ScaleMark.AlignmentPoint = new Point3d(-5, 18, 0);
+                        ScaleMark.Height = 2.5;
+                        ScaleMark.WidthFactor = 0.7;
+                        ScaleMark.HorizontalMode = TextHorizontalMode.TextMid;                       
+                        #endregion
+
 
                         BlockTableRecord BTR = Trans.GetObject(LT.BlockTableRecordId, OpenMode.ForWrite) as BlockTableRecord;
                         BTR.AppendEntity(VP);
@@ -174,6 +338,19 @@ namespace SetViewPort
                 }
 
             }
+            
+
+
+
+        }
+        [CommandMethod("restart")]
+        public void TestCommand() // This method can have any name
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+
+            Editor ed = doc.Editor;
+            ed.WriteMessage("\n中断了哦");
+
         }
     }
 
