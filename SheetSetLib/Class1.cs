0c2a0c7f8924566d0f3a70b70492dda42e95eb59
sonicg83 <50348011+sonicg83@users.noreply.github.com>
2021-11-10 23:02:04 +0800

    new

---
+++ SheetSetLib/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SheetSetLib
+{
+    /// <summary>
+    /// 描述图纸模型
+    /// </summary>
+    public class Sheet
+    {
+        #region Fields
+        private int _GlobalSerial;
+        private int _InnerSerial;
+        private readonly string _DivisionCode;
+        private string _SheetName;
+        private string _SheetSize;
+        private string _Scale;
+        private string _Remark;
+        private string _Draft;
+        private string _Design;
+        private string _Check;
+        private string _Chief;
+        
+
+
+        #endregion
+        #region Properties
+        public int GlobalSerial
+        {
+            get => _GlobalSerial;
+            set => _GlobalSerial = value;
+        }
+        #endregion
+
+        #region Methods
+        #endregion
+
+        #region Constructor
+        #endregion
+    }
+}
